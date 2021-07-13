b313fee570ecb2a39d50845d0431c332493dffd1
kingbri <bdashore3@gmail.com>
2021-07-13 17:05:58 -0400

    Test: Show broken TabView behavior
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- PagedTabViewTest/ContentView.swift
+++ PagedTabViewTest/ContentView.swift
@@ -7,15 +7,54 @@
 
 import SwiftUI
 
+struct Example: Hashable {
+    var index: Int
+    var showDummyView: Bool
+}
+
 struct ContentView: View {
+    @State private var selection = 0
+    private var testArray = [
+        Example(index: 1, showDummyView: false),
+        Example(index: 2, showDummyView: false),
+        Example(index: 3, showDummyView: false),
+        Example(index: 4, showDummyView: true)
+    ]
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView(selection: $selection) {
+            ForEach(testArray, id: \.self) { example in
+                if example.showDummyView {
+                    DummyView(index: example.index)
+                } else {
+                    ChildView(index: example.index)
+
+                }
+            }
+        }
+        .tabViewStyle(PageTabViewStyle())
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct ChildView: View {
+    var index: Int
+    
+    var body: some View {
+        Text("Element \(index)")
+        .onAppear {
+            print("Normal view appeared")
+        }
+    }
+}
+
+struct DummyView: View {
+    var index: Int
+    
+    var body: some View {
+        Text("This is a dummy view on index \(index)")
+            .onAppear {
+                print("Dummy view appeared")
+                print("Break")
+            }
     }
 }
